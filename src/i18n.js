634d0895e12a6c15275562c07b0f8c46c8deefdc
joe bloggs <joe bloggs@gmail.com>
2019-09-21 19:15:33 +0100

    fixed i18n bugs

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,17 @@
 import i18n from "i18n-js";
+import { NativeModules } from "react-native";
 
 import de from "./Locales/de.json";
 import en from "./Locales/en.json";
 
 i18n.defaultLocale = "en";
-i18n.locale = "en";
+
+try {
+  i18n.locale = NativeModules.I18nManager.localeIdentifier.split("_")[0];
+} catch (e) {
+  i18n.locale = "en";
+}
+
 i18n.fallbacks = true;
 i18n.translations = { de, en };
 
